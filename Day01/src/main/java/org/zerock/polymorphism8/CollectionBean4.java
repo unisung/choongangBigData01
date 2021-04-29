2efc05a81a932b119e364583b4afe95e2d12f4d7
unisung <vctor@naver.com>
2021-04-29 15:35:41 +0900

    3

---
+++ Day01/src/main/java/org/zerock/polymorphism8/CollectionBean4.java
@@ -0,0 +1,19 @@
+package org.zerock.polymorphism8;
+
+import java.util.Properties;
+
+public class CollectionBean4 {
+	
+	private Properties addressList;
+
+	public Properties getAddressList() {
+		return addressList;
+	}
+
+	public void setAddressList(Properties addressList) {
+		this.addressList = addressList;
+	}
+	
+	
+
+}
