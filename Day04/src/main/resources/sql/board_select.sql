f884b6a1622a6ab34b37c774cdf10ba28b84685b
unisung <vctor@naver.com>
2021-05-11 12:08:58 +0900

    1

---
+++ Day04/src/main/resources/sql/board_select.sql
@@ -0,0 +1,43 @@
+select * from board where title like '%'||'테스트'||'%' order by seq desc;
+select * from board where content like '%'||'테스트'||'%' order by seq desc;
+
+select *
+ from
+(select rownum rn, a.*
+ from 
+(select * 
+  from board 
+  where title like '%'||'파일'||'%'
+  order by seq desc) a)
+--where rn between 21 and 30
+where rn >= 1 and rn <= 10
+;
+
+select *
+ from
+(select rownum rn, a.*
+ from 
+(select * 
+  from board 
+  where content like '%'||'파일'||'%'
+  order by seq desc) a)
+--where rn between 21 and 30
+where rn >= 1 and rn <= 10
+;
+
+select *
+ from
+(select rownum rn, a.*
+ from 
+(select * 
+  from board 
+  order by seq desc) a)
+--where rn between 21 and 30
+where rn >= 11 and rn <= 20
+;
+페이지번호,  시작글번호,         끝 글 번호
+ 1:        (1-1)*10 +1=1,   10: 1 + 9 =10 
+ 2:        (2-1)*10 +1=11,  20: 11 + 9 =20
+ 3:        (3-1)*10 +1=21,  30: 21 + 9 =30
+        (페이지번호 -1)*페이지당 글 갯수 + 1, 끝 글 번호 = 시작글 번호 + 9 
+
