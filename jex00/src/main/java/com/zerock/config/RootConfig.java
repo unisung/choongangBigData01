bf2a467b22c4030eb374d199d44cae1fac2fa5c1
unisung <vctor@naver.com>
2021-05-25 09:46:57 +0900

    05-25-001.

---
+++ jex00/src/main/java/com/zerock/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.zerock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.zerock.sample"})
+public class RootConfig {
+
+}
