95a8935dc950507b82e1c73602c46c67ef57e6bd
unisung <vctor@naver.com>
2021-05-28 06:54:04 +0900

    b

--- ex00/src/main/java/org/zerock/controller/HomeController.java
+++ ex00/src/main/java/org/zerock/controller/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("aaaa");
 		return "home";
 	}
 	
