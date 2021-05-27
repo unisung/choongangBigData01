67d449df32e3585fff2e492abd39e9a4dbbbaad2
unisung <vctor@naver.com>
2021-05-27 11:23:55 +0900

    05-27-002.

--- ex01/src/main/java/org/zerock/exception/CommonExceptionAdvice.java
+++ ex01/src/main/java/org/zerock/exception/CommonExceptionAdvice.java
@@ -1,8 +1,11 @@
 package org.zerock.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j;
 
@@ -17,6 +20,12 @@ public class CommonExceptionAdvice {
 		model.addAttribute("exception",ex);
 		log.error(model);
 		return "error_page";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException ex) {
+		return "custom404";
 	}
 
 }
