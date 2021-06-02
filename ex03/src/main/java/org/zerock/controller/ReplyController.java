1cf2fe4a5d4da11943423c9cb2e593973f0acb42
unisung <vctor@naver.com>
2021-06-02 10:19:20 +0900

    06-02-001.

--- ex03/src/main/java/org/zerock/controller/ReplyController.java
+++ ex03/src/main/java/org/zerock/controller/ReplyController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
@@ -39,10 +41,49 @@ public class ReplyController {
 			  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	/* 댓글 리스트 출력 */
 	@GetMapping(value="/pages/{bno}/{page}",
-            produces= {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
-public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno){
+            produces= {MediaType.APPLICATION_JSON_UTF8_VALUE, 
+            		           MediaType.APPLICATION_XML_VALUE})
+public ResponseEntity<List<ReplyVO>> getList(
+														                 @PathVariable("page") int page, 
+														                 @PathVariable("bno") Long bno){
     Criteria cri = new Criteria(page, 10);
-    return new ResponseEntity<List<ReplyVO>>(service.getList(cri, bno), HttpStatus.OK);
+    return new ResponseEntity<List<ReplyVO>>(service.getList(cri, bno), 
+    		                                                               HttpStatus.OK);
 	}
+	
+	/* 댓글 조회 */
+	@GetMapping(value="/{rno}", produces= {MediaType.APPLICATION_JSON_VALUE, 
+			                                                         MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno){
+		log.info("get:" +rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	/*  댓글 삭제 */
+	@DeleteMapping(value="/{rno}", produces= {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+		log.info("remove: " + rno);
+		
+		return service.remove(rno)==1? new ResponseEntity<>("success",HttpStatus.OK)
+				:new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	/* 댓글 수정 */
+@RequestMapping(method= {RequestMethod.PUT, RequestMethod.PATCH}, /*전송방식*/
+                                              value="/{rno}", /* uri - pathvariable */
+                                             consumes="application/json",  /* 파라미터 타입*/
+                                             produces= {MediaType.TEXT_PLAIN_VALUE}) /* 응답결과 타입 */
+ public ResponseEntity<String> modify(@PathVariable("rno") Long rno,
+		                                                    @RequestBody ReplyVO vo){
+	vo.setRno(rno);
+	
+	log.info("rno: "+rno);
+	
+	return service.modify(vo)==1? new ResponseEntity<>("success",HttpStatus.OK)
+			: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+}
+	
 }
